3d2a98a086a80aeea3d347adec8b0d4247184f22
theodortruffer <tt@studer-raimann.ch>
2016-12-16 14:42:04 +0100

    updates for ilias 5.2

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 $id = 'swdr';
-$version = '1.0.0';
+$version = '1.0.1';
 $ilias_min_version = '4.4.0';
-$ilias_max_version = '5.0.999';
+$ilias_max_version = '5.2.999';
 $responsible = 'Theodor Truffer';
 $responsible_mail = 'tt@studer-raimann.ch';
 ?>
