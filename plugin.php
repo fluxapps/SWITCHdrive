7b023330fd8922b9727538cb103354daaddb67c7
Adrian Lüthi <al@studer-raimann.ch>
2019-03-29 12:28:07 +0100

    update plugin php for 5.4

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 $id = 'swdr';
 $version = '1.0.1';
 $ilias_min_version = '4.4.0';
-$ilias_max_version = '5.2.999';
+$ilias_max_version = '5.4.999';
 $responsible = 'Theodor Truffer';
 $responsible_mail = 'tt@studer-raimann.ch';
 ?>
