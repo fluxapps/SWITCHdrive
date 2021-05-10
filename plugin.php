4da46fcd9d883efdd90048fa3cddb19aa993b4c2
Fabian Schmid <fs@studer-raimann.ch>
2021-05-10 20:34:14 +0200

    version 6

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 $id = 'swdr';
 $version = '1.0.1';
 $ilias_min_version = '5.4.0';
-$ilias_max_version = '5.4.999';
+$ilias_max_version = '6.99';
 $responsible = 'Theodor Truffer';
 $responsible_mail = 'tt@studer-raimann.ch';
 ?>
