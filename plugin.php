3eeac6b387cdcd1a57d0e0366975b91cc2597759
fluxms <martin@fluxlabs.ch>
2022-03-22 03:06:01 +0100

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 $id = 'swdr';
 $version = '1.0.1';
-$ilias_min_version = '5.4.0';
-$ilias_max_version = '6.99';
-$responsible = 'Theodor Truffer';
-$responsible_mail = 'tt@studer-raimann.ch';
+$ilias_min_version = '6.0';
+$ilias_max_version = '7.99';
+$responsible = 'fluxlabs ag';
+$responsible_mail = 'support@fluxlabs.ch';
 ?>
